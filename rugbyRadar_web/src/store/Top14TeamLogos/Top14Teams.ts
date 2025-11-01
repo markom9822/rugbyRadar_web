44f544246746dbc5f9b5ed942ab8c4dfefe6ed44
Mark O Meara <markomeara98@gmail.com>
2025-11-01 17:09:51 +0000

    rest of team logos

---
+++ rugbyRadar_web/src/store/Top14TeamLogos/Top14Teams.ts
@@ -0,0 +1,50 @@
+export const ClermontLogo = require('@/store/Top14TeamLogos/ASM_Clermont_Logo.png')
+export const ClermontAltLogo = require('@/store/Top14TeamLogos/Clermont_Rugby_Logo_White.png')
+
+export const BayonneLogo = require('@/store/Top14TeamLogos/Aviron_Bayonnais_Logo.png')
+export const BayonneAltLogo = require('@/store/Top14TeamLogos/Bayonne_Rugby_Logo_White.png')
+
+export const BriveLogo = require('@/store/Top14TeamLogos/CA_Brive_Rugby_Logo.png')
+export const BriveAltLogo = require('@/store/Top14TeamLogos/Brive_Rugby_Logo_White.png')
+
+export const CastresLogo = require('@/store/Top14TeamLogos/Castres_olympique_logo.png')
+export const CastresAltLogo = require('@/store/Top14TeamLogos/Castres_Olympique_Logo_White.png')
+
+export const MontpellierLogo = require('@/store/Top14TeamLogos/Montpellier_Rugby_Logo.png')
+export const MontpellierAltLogo = require('@/store/Top14TeamLogos/Montpellier_Rugby_Logo_White.png')
+
+export const LyonLogo = require('@/store/Top14TeamLogos/Lyon_Olympique_Universitaire.png')
+export const LyonAltLogo = require('@/store/Top14TeamLogos/Lyon_Rugby_Logo_White.png')
+
+export const PauLogo = require('@/store/Top14TeamLogos/Pau_Rugby_Logo.png')
+export const PauAltLogo = require('@/store/Top14TeamLogos/Pau_Rugby_Logo_White.png')
+
+export const PerpignanLogo = require('@/store/Top14TeamLogos/Perpignan_logo.png')
+export const PerpignanAltLogo = require('@/store/Top14TeamLogos/Perpignan_Rugby_Logo_White.png')
+
+export const Racing92Logo = require('@/store/Top14TeamLogos/Racing_92_logo.png')
+export const Racing92AltLogo = require('@/store/Top14TeamLogos/Racing92_Rugby_Logo_White.png')
+
+export const ToulonLogo = require('@/store/Top14TeamLogos/RC_Toulon_Logo.png')
+export const ToulonAltLogo = require('@/store/Top14TeamLogos/Toulon_Rugby_Logo_White.png')
+
+export const VannesLogo = require('@/store/Top14TeamLogos/RC_Vannes_Logo.png')
+export const VannesAltLogo = require('@/store/Top14TeamLogos/Vannes_Rugby_Logo_White.png')
+
+export const StadeFrancaisLogo = require('@/store/Top14TeamLogos/Stade_francais_logo.png')
+export const StadeFrancaisAltLogo = require('@/store/Top14TeamLogos/Stade_Francais_Rugby_Logo_White.png')
+
+export const LaRochelleLogo = require('@/store/Top14TeamLogos/Stade_Rochelais_Logo.png')
+export const LaRochelleAltLogo = require('@/store/Top14TeamLogos/LaRochelle_Rugby_Logo_White.png')
+
+export const ToulouseLogo = require('@/store/Top14TeamLogos/Stade_Toulousain_Logo.png')
+export const ToulouseAltLogo = require('@/store/Top14TeamLogos/Toulouse_Rugby_Logo_White.png')
+
+export const BordeauxLogo = require('@/store/Top14TeamLogos/Union_Bordeaux_Begles_Logo.png')
+export const BordeauxAltLogo = require('@/store/Top14TeamLogos/Bordeaux_Rugby_Logo_White.png')
+
+export const OyonnaxLogo = require('@/store/Top14TeamLogos/oyonnax_logo.png')
+export const OyonnaxAltLogo = require('@/store/Top14TeamLogos/oyonnax_logo_white.png')
+
+export const MontaubanLogo = require('@/store/Top14TeamLogos/Us_Montauban_logo.png')
+export const MontaubanAltLogo = require('@/store/Top14TeamLogos/Us_Montauban_logo_white.png')
---
+++ rugbyRadar_web/src/store/TrophyIcons/TrophyIcons.ts
@@ -0,0 +1,11 @@
+export const URCTrophyIcon = require('@/store/TrophyIcons/urc_trophy.png')
+
+export const Top14TrophyIcon = require('@/store/TrophyIcons/top14_trophy.png')
+
+export const RWCTrophyIcon = require('@/store/TrophyIcons/rugby_world_cup_trophy.png')
+
+export const PremiershipTrophyIcon = require('@/store/TrophyIcons/premiership_trophy.png')
+
+export const ChampsCupTrophyIcon = require('@/store/TrophyIcons/champs_cup_trophy.png')
+
+export const ChallengeCupTrophyIcon = require('@/store/TrophyIcons/challenge_cup_trophy.png')
