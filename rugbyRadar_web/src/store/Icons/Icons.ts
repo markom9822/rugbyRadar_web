bba0231f455ea4a97b1b845cdbbd766cbf2990e3
Mark O Meara <markomeara98@gmail.com>
2025-11-01 17:07:12 +0000

    added in icons

---
+++ rugbyRadar_web/src/store/Icons/Icons.ts
@@ -0,0 +1,10 @@
+export const RugbyPostsIcon = require('@/store/Icons/Rugby_Posts.png')
+export const RugbyTryIcon = require('@/store/Icons/Rugby_Try.png')
+export const YellowCardIcon = require('@/store/Icons/Yellow_card.png')
+export const RedCardIcon = require('@/store/Icons/Red_card.png')
+export const DropGoalIcon = require('@/store/Icons/Drop_Goal.png')
+export const RefereeWhistleIcon = require('@/store/Icons/referee_whistle_icon.png')
+export const RugbyRadarIconWhite = require('@/store/Icons/RugbyRadar_AppIcon_WhiteTransparent.png')
+
+export const AllLeaguesIcon = require('@/store/Icons/AllLeaguesIcon.png')
+
