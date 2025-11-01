56b0db6a186cb9429a1bc623c7d4e329305f2c9a
Mark O Meara <markomeara98@gmail.com>
2025-11-01 17:07:41 +0000

    team logos

---
+++ rugbyRadar_web/src/store/InternationalTeamLogos/InternationalTeams.ts
@@ -0,0 +1,74 @@
+export const IrelandLogo = require('@/store/InternationalTeamLogos/Irish_Rugby_Football_Union_logo.png')
+export const IrelandAltLogo = require('@/store/InternationalTeamLogos/Irish_Rugby_Logo_White.png')
+
+export const SouthAfricaLogo = require('@/store/InternationalTeamLogos/South_Africa_Rugby_Logo.png')
+export const SouthAfricaAltLogo = require('@/store/InternationalTeamLogos/South_Africa_Rugby_Logo_White.png')
+
+export const EnglandLogo = require('@/store/InternationalTeamLogos/England_national_rugby_team_logo.png')
+export const EnglandAltLogo = require('@/store/InternationalTeamLogos/England_Rugby_Logo_White.png')
+
+export const NewZealandLogo = require('@/store/InternationalTeamLogos/All_Blacks_logo_silver.png')
+export const NewZealandAltLogo = require('@/store/InternationalTeamLogos/All_Blacks_Logo_White.png')
+
+export const AustraliaLogo = require('@/store/InternationalTeamLogos/Logo_Wallabies.png')
+export const AustraliaAltLogo = require('@/store/InternationalTeamLogos/Australia_Rugby_Logo_White.png')
+
+export const WalesLogo = require('@/store/InternationalTeamLogos/Welsh_Rugby_Union_logo.png')
+export const WalesAltLogo = require('@/store/InternationalTeamLogos/Wales_Rugby_Logo_White.png')
+
+export const FranceLogo = require('@/store/InternationalTeamLogos/France_Rugby_logo.png')
+export const FranceAltLogo = require('@/store/InternationalTeamLogos/France_Rugby_White_Logo.png')
+
+export const ScotlandLogo = require('@/store/InternationalTeamLogos/Scotlandlogo.png')
+export const ScotlandAltLogo = require('@/store/InternationalTeamLogos/Scotland_Logo_White.png')
+
+export const ArgentinaLogo = require('@/store/InternationalTeamLogos/Los_pumas_argentina_logo.png')
+export const ArgentinaAltLogo = require('@/store/InternationalTeamLogos/Los_Pumas_Logo_White.png')
+
+export const ItalyLogo = require('@/store/InternationalTeamLogos/Italian_Rugby_Federation_logo.png')
+export const ItalyAltLogo = require('@/store/InternationalTeamLogos/Italy_Rugby_Logo_White.png')
+
+export const FijiLogo = require('@/store/InternationalTeamLogos/Fiji_Rugby_Logo_silver.png')
+export const FijiAltLogo = require('@/store/InternationalTeamLogos/Fiji_Rugby_Logo_White.png')
+
+export const JapanLogo = require('@/store/InternationalTeamLogos/Logo_Japan_Rugby.png')
+export const JapanAltLogo = require('@/store/InternationalTeamLogos/Japan_Rugby_Logo_White.png')
+
+export const GeorgiaLogo = require('@/store/InternationalTeamLogos/Georgian_Rugby_Union.png')
+export const GeorgiaAltLogo = require('@/store/InternationalTeamLogos/Georgia_Rugby_Logo_White.png')
+
+export const PortugalLogo = require('@/store/InternationalTeamLogos/Portugal_Rugby_Logo.png')
+export const PortugalAltLogo = require('@/store/InternationalTeamLogos/Portugal_Rugby_Logo_White.png')
+
+export const CanadaLogo = require('@/store/InternationalTeamLogos/Rugby_Canada_logo.png')
+export const CanadaAltLogo = require('@/store/InternationalTeamLogos/Canada_Rugby_Logo_White.png')
+
+export const USALogo = require('@/store/InternationalTeamLogos/United_States_national_rugby_union_team_logo.png')
+export const USAAltLogo = require('@/store/InternationalTeamLogos/USA_Rugby_Logo_White.png')
+
+export const ChileLogo = require('@/store/InternationalTeamLogos/Condores_Chile_Rugby.png')
+export const ChileAltLogo = require('@/store/InternationalTeamLogos/Chile_Rugby_Logo_White.png')
+
+export const NamibiaLogo = require('@/store/InternationalTeamLogos/Namibia_Rugby_Logo.png')
+export const NamibiaAltLogo = require('@/store/InternationalTeamLogos/Namibia_Rugby_Logo_White.png')
+
+export const TongaLogo = require('@/store/InternationalTeamLogos/Logo_Tonga_Rugby.png')
+export const TongaAltLogo = require('@/store/InternationalTeamLogos/Tonga_Rugby_Logo_White.png')
+
+export const SamoaLogo = require('@/store/InternationalTeamLogos/Logo_Samoa_Rugby.png')
+export const SamoaAltLogo = require('@/store/InternationalTeamLogos/Samoa_Rugby_Logo_White.png')
+
+export const RomaniaLogo = require('@/store/InternationalTeamLogos/Romania_national_rugby_union_team_logo.png')
+export const RomaniaAltLogo = require('@/store/InternationalTeamLogos/Romania_Rugby_Logo_White.png')
+
+export const SpainLogo = require('@/store/InternationalTeamLogos/Spain_Rugby_logo.png')
+export const SpainAltLogo = require('@/store/InternationalTeamLogos/Spain_Rugby_Logo_White.png')
+
+export const UruguayLogo = require('@/store/InternationalTeamLogos/Uruguay_Los_teros_logo.png')
+export const UruguayAltLogo = require('@/store/InternationalTeamLogos/Uruguay_Rugby_Logo_White.png')
+
+export const BALionsLogo = require('@/store/InternationalTeamLogos/British_&_Irish_Lions_logo.png')
+export const BALionsAltLogo = require('@/store/InternationalTeamLogos/British_&_Irish_Lions_Logo_White.png')
+
+export const DefaultLogo = require('@/store/InternationalTeamLogos/Rugby_Default_Crest.png')
+
