c9f7a9613b90497072799e0166ba3fe9537e7548
Mark O Meara <markomeara98@gmail.com>
2025-11-01 17:08:22 +0000

    league logos

---
+++ rugbyRadar_web/src/store/LeagueLogos/LeagueLogos.ts
@@ -0,0 +1,41 @@
+export const ChampionsCupLogo = require('@/store/LeagueLogos/Champions_Cup_Logo.png')
+export const ChampionsCupAltLogo = require('@/store/LeagueLogos/Champions_Cup_Logo_White.png')
+
+export const ChallengeCupLogo = require('@/store/LeagueLogos/Challenge_Cup_Logo.png')
+export const ChallengeCupAltLogo = require('@/store/LeagueLogos/Challenge_Cup_Logo_White.png')
+
+export const PremiershipLogo = require('@/store/LeagueLogos/Prem_Logo.png')
+export const PremiershipAltLogo = require('@/store/LeagueLogos/Prem_Logo_White.png')
+
+export const RugbyChampLogo = require('@/store/LeagueLogos/Rugby_Championship_Logo.png')
+export const RugbyChampAltLogo = require('@/store/LeagueLogos/Rugby_Championship_Logo_White.png')
+
+export const WorldCupLogo = require('@/store/LeagueLogos/Rugby_World_Cup_Logo.png')
+export const WorldCupAltLogo = require('@/store/LeagueLogos/Rugby_World_Cup_Logo_White.png')
+
+export const SixNationsLogo = require('@/store/LeagueLogos/Six_Nations_Logo.png')
+export const SixNationsAltLogo = require('@/store/LeagueLogos/Six_Nations_Logo_White.png')
+
+export const SuperRugbyLogo = require('@/store/LeagueLogos/Super_Rugby_Logo.png')
+export const SuperRugbyAltLogo = require('@/store/LeagueLogos/Super_Rugby_Logo_White.png')
+
+export const Top14Logo = require('@/store/LeagueLogos/Top_14_Logo.png')
+export const Top14AltLogo = require('@/store/LeagueLogos/Top_14_Logo_White.png')
+
+export const URCLogo = require('@/store/LeagueLogos/URC_Logo.png')
+export const URCAltLogo = require('@/store/LeagueLogos/URC_Logo_White.png')
+
+export const RankingsLogo = require('@/store/LeagueLogos/Rankings_Icon.png')
+export const InternationalLogo = require('@/store/LeagueLogos/International_Icon.png')
+
+export const AutumnNationsLogo = require('@/store/LeagueLogos/Autumn_Nations_Logo.png')
+export const AutumnNationsAltLogo = require('@/store/LeagueLogos/Autumn_Nations_Logo_White.png')
+
+export const U20SixNationsLogo = require('@/store/LeagueLogos/u20_SixNations_Logo.png')
+export const U20SixNationsAltLogo = require('@/store/LeagueLogos/u20_SixNations_Logo_White.png')
+
+export const U20WorldChampsLogo = require('@/store/LeagueLogos/u20_world_champs_logo.png')
+export const U20WorldChampsAltLogo = require('@/store/LeagueLogos/u20_world_champs_logo_white.png')
+
+export const PacificNationsCupLogo = require('@/store/LeagueLogos/pacific_nations_logo.png')
+export const PacificNationsCupAltLogo = require('@/store/LeagueLogos/pacific_nations_logo_white.png')
