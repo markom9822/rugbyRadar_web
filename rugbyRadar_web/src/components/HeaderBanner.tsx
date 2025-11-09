79e0a1a36e517d1c461274f8405f830d2baa8e02
Mark O Meara <markomeara98@gmail.com>
2025-11-09 11:38:42 +0000

    added links to header banner

--- rugbyRadar_web/src/components/HeaderBanner.tsx
+++ rugbyRadar_web/src/components/HeaderBanner.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './HeaderBanner.css';
 import RugbyRadarLogo from '/RugbyRadar_AppIcon_WhiteTransparent.png';
 
@@ -6,8 +7,16 @@ export const HeaderBanner = () => {
 
     return (
         <div className="header-banner">
-            <img src={RugbyRadarLogo}/>
-            <p>Rugby  Radar</p>
+            <div className='logo-banner'>
+                <img src={RugbyRadarLogo}/>
+                <p>Rugby  Radar</p>
+            </div>
+
+            <div className='nav-banner'>
+                <Link to="/">Fixtures</Link>
+                <Link to="/teams">Teams</Link>
+                <Link to="/leagues">Leagues</Link>
+            </div>
         </div>
     )
 }
