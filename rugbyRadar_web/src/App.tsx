fc6cf6533e26c73bbe936f274fc66c89cf4bd8d1
Mark O Meara <markomeara98@gmail.com>
2025-11-09 18:37:36 +0000

    fix up

--- rugbyRadar_web/src/App.tsx
+++ rugbyRadar_web/src/App.tsx
@@ -1,22 +1,28 @@
-import {Route, Routes } from 'react-router-dom'
+import {Outlet, Route, Routes } from 'react-router-dom'
 import './App.css'
 import { HeaderBanner } from './components/HeaderBanner'
 import { FixturesPage } from './pages/FixturesPage'
 import { TeamsPage } from './pages/TeamsPage'
 import { LeaguesPage } from './pages/LeaguesPage'
 
+const Layout = () => (
+  <div style={{ padding: "1rem", backgroundColor: "#f0f0f0"}}>
+    <HeaderBanner />
+    {/* Other consistent elements can go here */}
+    <Outlet />
+  </div>
+);
+
 function App() {
 
   return (
-    <>
-      <HeaderBanner/>
-
-        <Routes>
-          <Route path="/" element={<FixturesPage />} />
-          <Route path="/teams" element={<TeamsPage />} />
-          <Route path="/leagues" element={<LeaguesPage />} />
-        </Routes>
-    </>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<FixturesPage />} />
+        <Route path="/teams" element={<TeamsPage />} />
+        <Route path="/leagues" element={<LeaguesPage />} />
+      </Route>
+    </Routes>
   )
 }
 
